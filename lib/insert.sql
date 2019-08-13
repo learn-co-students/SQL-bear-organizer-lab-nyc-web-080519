2afb2d78d33749a016a0ebd657b9b64bf7f178eb
Mason Balbera <mhbalbera@gmail.com>
2019-08-13 15:14:50 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate",13,"male","brown","friendly",true);
+INSERT INTO bears VALUES (2,"Rowdy",3,"male","black","unfriendly",true);
+INSERT INTO bears VALUES (3,"Tabitha",8,"female","burgandy","kinda-friendly",true);
+INSERT INTO bears VALUES (4,"Sergeant Brown",37,"female","brown","hostile",true);
+INSERT INTO bears VALUES (5,"Melissa",77,"female","magenta","shy",true);
+INSERT INTO bears VALUES (6,"Grinch",25,"male","green","agressively ruins christmas",true);
+INSERT INTO bears VALUES (7,"Wendy",19,"female","polar","friendly",true);
+INSERT INTO bears VALUES (8,null, 3,"male","brown","known killer",true);
