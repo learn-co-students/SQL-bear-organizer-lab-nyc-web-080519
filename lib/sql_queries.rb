12a53be4575e099b6568f1b064d4134f6a958f2e
Darrow Palast <dpalast@gmail.com>
2019-08-14 13:36:24 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ end
 
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  # "SELECT COUNT(color) "
+  
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -31,5 +31,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name IS null"
 end
